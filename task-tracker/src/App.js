de8043ef1ad119bbedd0cf8218f284f1aa962cdd
Isa SUMER <sumerisa5@gmail.com>
2021-09-15 23:45:39 +0300

    app js created

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from './components/Button';
+import Header from './components/Header';
+import Tasks from './components/Tasks.jsx';
 
 function App() {
   return (
     <div className="container">
-      <h1>Task Tracker</h1>
+      <Header title= "Task Tracker"/>
+      <Button color="purple" text="Show Create a Task Bar"/>
+      <Tasks/>
     </div>
   );
+}
+
+Header.defaultProps = {
+  title:"Task Tracker as default"
 }
 
 export default App;
