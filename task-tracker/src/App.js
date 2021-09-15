b123452e6dc82740156c91b652baa0e79c596dbb
Isa SUMER <sumerisa5@gmail.com>
2021-09-15 16:48:58 +0300

    app created

---
+++ task-tracker/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Task Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
