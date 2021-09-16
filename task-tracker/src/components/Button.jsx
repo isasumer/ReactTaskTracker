297b10c1048abd1fd47337165918221648b3f769
Isa SUMER <sumerisa5@gmail.com>
2021-09-16 14:35:16 +0300

    click event added

--- task-tracker/src/components/Button.jsx
+++ task-tracker/src/components/Button.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-const Button = ({ text, color }) => {
+const Button = ({ text, color, toggleShow }) => {
   return (
     <div>
-      <button className="btn" style={{ backgroundColor: color }}>
+      <button
+        className="btn"
+        style={{ backgroundColor: color }}
+        onClick={toggleShow}
+      >
         {text}
       </button>
     </div>
