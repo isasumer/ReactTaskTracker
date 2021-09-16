4cd582d8ee14212580f1365c64571e95cd70167a
Isa SUMER <sumerisa5@gmail.com>
2021-09-16 14:34:48 +0300

    button onclick event added

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,27 +1,51 @@
 import "./App.css";
-import Button from "./components/Button";
 import Header from "./components/Header";
-import Tasks from "./components/Tasks.jsx";
+import TaskList from "./components/TaskList.jsx";
+import CreateTask from "./components/CreateTask.jsx";
 import { useState } from "react";
 import { initialState } from "./store/initialStates";
 
 function App() {
   const [tasks, setTasks] = useState(initialState);
+  const [isTaskBarShowed, setIsTaskBarShowed] = useState(false);
 
-//  Create Task
+  //  Create Task
+  const onCreate = (task) => {
+    const id = Date.now();
+    const newTask = { id, ...task };
+    setTasks((prevState) => [...prevState, newTask]);
+  };
 
-//  Delete Task
-const onDelete = (deletedItemID) => setTasks(tasks.filter((task) => task.id !==id))
+  //  Delete Task
+  const onDelete = (deletedItemID) =>
+    setTasks(tasks.filter((task) => task.id === !deletedItemID));
 
-//  Toggle Task
+  //  Toggle Done
 
-//  Toggle Show and Hide
+  const toggleDone = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, isDone: !task.isDone } : task
+      )
+    );
+  };
+
+  //  Toggle Show and Hide
+  const toggleShow = () => setIsTaskBarShowed((prevState) => !prevState);
 
   return (
     <div className="container">
-      <Header title="Task Tracker" />
-      <Button color="purple" text="Show Create a Task Bar" />
-      <Tasks tasks = {tasks} onDelete= {onDelete}/>
+      <Header
+        title="Task Tracker"
+        isTaskBarShowed={isTaskBarShowed}
+        toggleShow={toggleShow}
+      />
+      {isTaskBarShowed && <CreateTask onCreate={onCreate} />}
+      {tasks.length > 0 ? (
+        <TaskList tasks={tasks} onDelete={onDelete} toggleDone={toggleDone} />
+      ) : (
+        <p>No Task to show</p>
+      )}
     </div>
   );
 }
