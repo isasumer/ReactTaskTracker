9a4f72aefc5c3f53707fdfda7c4fc99476280cba
Isa SUMER <sumerisa5@gmail.com>
2021-09-16 14:35:41 +0300

    header upgraded

--- task-tracker/src/components/Header.jsx
+++ task-tracker/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({ title }) => {
+const Header = ({ title, isTaskBarShowed, toggleShow }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
+      <Button
+        color={isTaskBarShowed ? "steelblue" : "purple"}
+        text={isTaskBarShowed ? "Hide Create a Task Bar" : "Show Create a Task Bar"}
+        toggleShow = {toggleShow}
+      />
     </header>
   );
 };
