166d124b3f572c38e7959318d78cf489a91e6096
Isa SUMER <sumerisa5@gmail.com>
2021-09-16 14:35:26 +0300

    task created

---
+++ task-tracker/src/components/CreateTask.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function CreateTask({ onCreate }) {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+  const handleDayChange = (e) => {
+    setDay(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();   //sayfanın refresh olmasını engeller.
+    if (!text || !day) {
+      alert("Please fill both fields");
+    } else {
+      onCreate ({text,day, isDone:false})
+      setText("");
+      setDay("");
+    }
+  };
+
+  return (
+    <div>
+      <form className="add-form" onSubmit= {onSubmit}>
+        <div className="form-control">
+          <label htmlFor="task">Task</label>
+          <input
+            placeholder="Write a Task"
+            id="task"
+            name="text"
+            type="text"
+            value={text}
+            onChange={handleTextChange}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="day">Day & Time</label>
+          <input
+            placeholder="Enter a Date and Time"
+            id="day"
+            name="day"
+            type="text"
+            value={day}
+            onChange={handleDayChange}
+          />
+        </div>
+        <input type="submit" value="Save Task" className="btn btn-block" />
+      </form>
+    </div>
+  );
+}
+
+export default CreateTask;
